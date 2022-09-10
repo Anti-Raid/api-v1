d65517a61aee06b99f54a9a2186b7ca1fb35eb1a
whiteewolf <arog0660@gmail.com>
2022-09-10 15:50:55 +0300

    made some changes to get user and removed version check

--- endpoints/getuser.js
+++ endpoints/getuser.js
@@ -1,18 +1,15 @@
 module.exports = {
-	name: "users/get",
+	name: "user/get",
 	method: "GET",
-<<<<<<< HEAD
 	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
-		fetch(`https://discord.com/api/v10/users/775855009421066262`, {
+		// have to refactor this so it can run with not just putting id in the code but in the url
+		fetch(`https://discord.com/api/v10/users/${req.params.id}`, {
 			method: "GET",
 			headers: {
-				Authorization: 'Bot bot-token-here'
+				Authorization: 'Bot ODQ5MzMxMTQ1ODYyMjgzMjc1.YLZnRA.GOd92__QEBiBjGZDEhgMONOjwGg'
 			}
 		}).then(async resp => {
 			res.send(await resp.json())
 		})
 	},
-=======
-	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {},
->>>>>>> 5b635a2e6bdabbb35794d95c0b5dabd4fe12bd31
 };
