e1688e6371170897cd795603e60601345af4a351
selectthegang <recoyzane24@gmail.com>
2022-09-10 19:44:03 -0500

    Made some changes to the users schema

--- database/schemas/users.js
+++ database/schemas/users.js
@@ -2,10 +2,10 @@ const { Schema } = require("mongoose");
 
 const schema = new Schema({
 	id: String,
-	user: Array,
+	discordUser: Array,
 	guilds: Array,
 	notifications: Array,
-	devices: Array
+	tokens: Array
 });
 
 module.exports = schema;
