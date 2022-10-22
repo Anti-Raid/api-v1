37c7d319b92b52a5bf5a509a4d00aef81ef058d3
whiteewolf <arog0660@gmail.com>
2022-10-22 18:20:39 +0300

    added db thigs, some endpoints, updated a little api home page

--- endpoints/getuser.js
+++ endpoints/getuser.js
@@ -1,15 +1,11 @@
 module.exports = {
-	name: "user/get",
+	name: "users/get",
 	method: "GET",
 	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
+		// res.sendStatus(403)
+		const id = req.query.id;
 		// have to refactor this so it can run with not just putting id in the code but in the url
-		fetch(`https://discord.com/api/v10/users/${req.params.id}`, {
-			method: "GET",
-			headers: {
-				Authorization: 'Bot ODQ5MzMxMTQ1ODYyMjgzMjc1.YLZnRA.GOd92__QEBiBjGZDEhgMONOjwGg'
-			}
-		}).then(async resp => {
-			res.send(await resp.json())
-		})
+		const user = await database.getUser(id);
+		res.send(user)
 	},
 };
---
+++ endpoints/post_guilds_to_botlists.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "guilds/post",
+    method: "POST",
+    execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
+       res.send("This endpoint is not yet implemented.");
+    }
+}
