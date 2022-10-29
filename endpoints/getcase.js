9f6ecff10eda2c0daeb703b562ecf278fa0ca4ce
whiteewolf <arog0660@gmail.com>
2022-10-30 00:20:37 +0300

    updated all endpoints to use new db

--- endpoints/getcase.js
+++ endpoints/getcase.js
@@ -1,10 +1,30 @@
+const db = require("../database/handler");
 module.exports = {
 	name: "case/get",
 	method: "GET",
+	/**
+	 *
+	 * @param {*} req
+	 * @param {*} res
+	 * @param {*} fetch
+	 * @param {db} database
+	 * @param {*} auth
+	 * @param {*} DOMpurify
+	 * @param {*} marked
+	 */
 	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
 		const guild = req.query.gid;
 		const caseid = req.query.cid;
-		const casee = await database.getCase(guild, caseid);
-		res.send(casee);
+		const casee = await database.Cases.getCase(guild, String(caseid));
+		if (casee) res.send(casee);
+		else
+			res.status(404).send({
+				message:
+					"We couldn't find any case with this ID within our database",
+				caseID: caseid,
+				guildID: guild,
+				error: true,
+				fatal: false,
+			});
 	},
 };
--- endpoints/post_guilds_to_botlists.js
+++ endpoints/post_guilds_to_botlists.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: "guilds/post",
 	method: "GET",
 	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
-		res.status(409);
-		res.send("This endpoint is not yet implemented.");
+		res.send("This endpoint is not yet implemented. And it may not");
 	},
 };
