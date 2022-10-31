0f84261db019139b602be82103af081022cc168b
selectdev <66577268+selectdev@users.noreply.github.com>
2022-10-31 12:04:08 -0500

    Removed "post_guilds_to_botlists" endpoint

--- endpoints/post_guilds_to_botlists.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: "guilds/post",
-	method: "GET",
-	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
-		res.send("This endpoint is not yet implemented. And it may not");
-	},
-};
