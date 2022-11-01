3ef1d1d383bc1b2b23f7977a02dab6e27401a8f1
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-01 01:00:47 -0500

    Improved code design

--- endpoints/getuser.js
+++ endpoints/getuser.js
@@ -14,7 +14,8 @@ module.exports = {
 	 */
 	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
 		const id = req.query.id;
-		const user = await database.User.getUser(id);
+		const user = await database.Users.getUser(id);
+        
 		if (user) res.send(user);
 		else
 			res.status(404).send({
