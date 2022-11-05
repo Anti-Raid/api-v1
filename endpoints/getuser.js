8140eecf8dacd88d3e95f2563f9c14b3d93a94e3
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-04 21:05:40 -0500

    Update getuser.js

--- endpoints/getuser.js
+++ endpoints/getuser.js
@@ -18,7 +18,8 @@ module.exports = {
 
 		user["tokens"] = [];
 		user["staff_applications"] = [];
-		if (user) res.send(user);
+		
+                if (user) res.send(user);
 		else
 			res.status(404).send({
 				message:
