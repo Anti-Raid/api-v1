e59ec35fac9a073758d586f0d8f1e75a98e42b12
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-06 18:07:40 -0600

    Development (#22)
    * fix to cdn + added new logo
    
    * .
    
    * Update getuser.js
    
    * Added "marketplace" to whitelisted origins
    
    This commit adds `https://marketplace.antiraid.xyz/` to our list of whitelisted origins for oAuth access.
    
    * Pulled new database commit
    
    * Possible fix for issue with oAuth
    
    * Formatted code
    
    Co-authored-by: whiteewolf <arog0660@gmail.com>

--- endpoints/getuserwithtoken.js
+++ endpoints/getuserwithtoken.js
@@ -14,7 +14,18 @@ module.exports = {
 	 */
 	execute: async (req, res, fetch, database, auth, DOMpurify, marked) => {
 		const token = req.query.token;
-		const user = await database.Users.getUserWithToken(token);
+		const tokenInfo = await database.Tokens.getToken(token);
+
+		if (!tokenInfo)
+			res.status(404).send({
+				message:
+					"We couldn't fetch any information about this user in our database",
+				token: token,
+				error: true,
+				fatal: false,
+			});
+
+		const user = await database.Users.getUser(tokenInfo.userID);
 
 		if (user) res.send(user);
 		else
