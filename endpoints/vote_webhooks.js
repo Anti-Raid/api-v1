afa71656653de8d007ac3392846d025925f5dde6
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-30 16:21:02 -0600

    Development (#31)
    * fix to cdn + added new logo
    
    * .
    
    * Update getuser.js
    
    * Added "marketplace" to whitelisted origins
    
    This commit adds `https://marketplace.antiraid.xyz/` to our list of whitelisted origins for oAuth access.
    
    * Pulled new database commit
    
    * Possible fix for issue with oAuth
    
    * Formatted code
    
    * Pulled new database commit
    
    * fixed token
    
    * add ratelimits
    
    * prob fix
    
    * vote_webhook system for ibl done
    
    * fix prob
    
    * Fixed
    
    * Formatted code
    
    * Pulled new database commit
    
    * Installed "infinity-bots"
    
    * Pulled database commit
    
    * Updated ratelimit max
    
    * Pulled database commit
    
    * Added blog endpoints
    
    * f
    
    * Add vote
    
    * Updated vote webhook
    
    Co-authored-by: whiteewolf <arog0660@gmail.com>
    Co-authored-by: Mizchiev Dev <58121837+whiteewolf@users.noreply.github.com>

--- endpoints/vote_webhooks.js
+++ endpoints/vote_webhooks.js
@@ -17,24 +17,35 @@ module.exports = {
 	 */
 	execute: webhook.hookListener(
 		async (vote, req, res, fetch, database, auth, DOMpurify, marked) => {
-			/*const embed = {
-                title: "New Vote!",
-                author: {
-                    name: "",
-                    icon_url: "",
-                    url: ""
-                }
-            };*/
+			const embed = {
+				title: "New Vote!",
+				author: {
+					name: `${vote.userObj.username}#${vote.userObj.discriminator}`,
+					icon_url: vote.userObj.avatar,
+					url: vote.userObj.avatar,
+				},
+				fields: [
+					{
+						name: "Service",
+						value: "Infinity Bot List",
+					},
+					{
+						name: "Vote Time",
+						value: String(new Date(vote.timeStamp)),
+					},
+					{
+						name: "Total Votes",
+						value: String(vote.votes),
+					},
+				],
+			};
 
-            console.log(vote);
-            
-            await fetch(
+			await fetch(
 				`https://discord.com/api/v9/channels/1045460117287616552/messages`,
 				{
 					method: "post",
 					body: JSON.stringify({
-						/*embeds: [embed]*/
-                        content: "test"
+						embeds: [embed]
 					}),
 					headers: {
 						Authorization: `Bot ODQ5MzMxMTQ1ODYyMjgzMjc1.YLZnRA.GOd92__QEBiBjGZDEhgMONOjwGg`,
