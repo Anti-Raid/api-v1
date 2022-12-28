6756e783cb515a15e814c68460acbd06fe8af468
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-27 20:34:22 -0600

    Delete linked-roles directory

--- linked-roles/storage.js
+++
@@ -1,9 +0,0 @@
-const store = new Map();
-
-const storeDiscordTokens = async (userId, tokens) => {
-  await store.set(`discord-${userId}`, tokens);
-}
-
-const getDiscordTokens = async (userId) => {
-  return store.get(`discord-${userId}`);
-}
